ae0062e26e3e3800a1d9ae1629147a1be6e0061f
yuvraj <yuvraj_2300@hotmail.com>
2018-09-23 22:57:36 +0530

    update

--- SpringMvcTest/src/main/java/com/learn/mvc/config/WebConfig.java
+++ SpringMvcTest/src/main/java/com/learn/mvc/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.ui.context.support.ResourceBundleThemeSource;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -14,6 +15,8 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.theme.CookieThemeResolver;
+import org.springframework.web.servlet.theme.ThemeChangeInterceptor;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
@@ -24,7 +27,7 @@ public class WebConfig implements WebMvcConfigurer {
 	// DECLARING THE STATIC RESOURCES
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/").setCachePeriod(31556926);
+		registry.addResourceHandler("/resources/**").addResourceLocations("/WebContent/").setCachePeriod(31556926);
 	}
 
 	@Bean
@@ -58,7 +61,8 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		localeChangeInterceptor();
+		registry.addInterceptor(localeChangeInterceptor());
+//		registry.addInterceptor(themeChangeInterceptor());
 	}
 
 	@Bean
@@ -68,7 +72,13 @@ public class WebConfig implements WebMvcConfigurer {
 		localeInterceptor.setParamName("lang");
 		return localeInterceptor;
 	}
-
+	
+/*	//ADDED THEMECHANGE
+	@Bean
+	ThemeChangeInterceptor	themeChangeInterceptor() {
+		return	new	ThemeChangeInterceptor();
+	}
+*/
 	@Bean
 	CookieLocaleResolver localeResolver() {
 		CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
@@ -78,4 +88,18 @@ public class WebConfig implements WebMvcConfigurer {
 		return cookieLocaleResolver;
 	}
 
+/*	@Bean
+	ResourceBundleThemeSource themeSource() {
+		return new ResourceBundleThemeSource();
+	}
+*/
+	/*@Bean
+	CookieThemeResolver themeResolver() {
+		CookieThemeResolver cookieThemeResolver = new CookieThemeResolver();
+		cookieThemeResolver.setDefaultThemeName("standard");
+		cookieThemeResolver.setCookieMaxAge(3600);
+		cookieThemeResolver.setCookieName("theme");
+		return cookieThemeResolver;
+	}
+*/
 }
