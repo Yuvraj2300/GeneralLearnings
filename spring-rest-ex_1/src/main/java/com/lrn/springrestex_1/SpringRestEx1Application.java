c9ba63f4f8c18556cd6b3c440a647a780b716ca2
ys19299 <ys19299@DE-YS19299.zs.local>
2019-01-04 11:37:19 +0530

    add main class property in the POM

--- spring-rest-ex_1/src/main/java/com/lrn/springrestex_1/SpringRestEx1Application.java
+++ spring-rest-ex_1/src/main/java/com/lrn/springrestex_1/SpringRestEx1Application.java
@@ -2,8 +2,10 @@ package com.lrn.springrestex_1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringRestEx1Application {
 
 	public static void main(String[] args) {
