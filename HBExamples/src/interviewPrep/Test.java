450281899fe9b1ec733892f49985fa1639d8e435
ys19299 <ys19299@DE-YS19299.zs.local>
2019-01-16 19:10:41 +0530

    Pushing a Spring Boot Eureka Server Application..

---
+++ HBExamples/src/interviewPrep/Test.java
@@ -0,0 +1,11 @@
+package interviewPrep;
+
+public class Test {
+	public static void main(String[] args) {
+		int	i;
+		String	n;
+		float	f;
+		double d1= 1_33.23;
+	//	System.out.println(f);
+	}
+}
